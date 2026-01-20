0a07e38471e010a26fb5ff555385748a91d6f571
silentbuddha1 <ramchandrakarki231@gmail.com>
2026-01-21 00:15:48 +0545

    express

--- Express/Redux/src/index.ts
+++ Express/Redux/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import type { Request, Response } from "express";
+import
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,9 +9,7 @@ const app = express();
 
 const PORT: number = Number(process.env.PORT);
 
-app.use("/user", (req: Request, res: Response) => {
-  res.send("Hello");
-});
+app.use("/api/dashboard", dashboardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

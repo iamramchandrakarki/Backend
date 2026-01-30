ac5194a9c5eafee741236d1569f3d49b4d440a19
silentbuddha1 <ramchandrakarki231@gmail.com>
2026-01-30 15:32:05 +0545

    employee module

---
+++ Nestjs/learning/src/employee/employee.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { EmployeeController } from './employee.controller';
+import { EmployeeService } from './employee.service';
+
+@Module({
+  providers: [EmployeeService],
+  controllers: [EmployeeController],
+})
+export class EmployeeModule {}
